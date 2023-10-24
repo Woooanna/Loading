626d791a19bc645fcfdd54a05cd68c2aba93d688
Yoanna <y.mareva@tinqin.com>
2023-10-24 13:59:17 +0300

    Default image added

--- Sources/Loading/LoadingView.swift
+++ Sources/Loading/LoadingView.swift
@@ -1,6 +1,6 @@
 //
 //  SwiftUIView.swift
-//  
+//
 //
 //  Created by Yoanna Mareva on 24.10.23.
 //
@@ -11,7 +11,13 @@ struct LoadingView: View {
     @ObservedObject var viewModel: LoadingViewModel
     
     var body: some View {
-        Text(viewModel.message)
+        Color.clear.ignoresSafeArea()
+            .overlay {
+                VStack{
+                    viewModel.image
+                    Text(viewModel.message)
+                }
+            }
     }
 }
 
--- Sources/Loading/LoadingViewModel.swift
+++ Sources/Loading/LoadingViewModel.swift
@@ -3,10 +3,10 @@ import SwiftUI
 
 public class LoadingViewModel: ObservableObject {
     @Published var message: String
-    private var image: Image?
+    @Published var image: Image
     
     init(message: String? = nil, image: Image? = nil) {
         self.message = message ?? "Loading ... "
-        self.image = image
+        self.image = image ?? Image("loading", bundle: .module)
     }
 }
